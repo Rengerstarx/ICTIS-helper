8002902bd8dee79511c8ff1ea5a85c75df30ea5c
Rengerstarx <78647558+Rengerstarx@users.noreply.github.com>
2022-11-23 01:15:14 +0300

    Map Update (Dinamic)

--- app/src/main/java/com/example/ictis/MapActivityD.kt
+++ app/src/main/java/com/example/ictis/MapActivityD.kt
@@ -19,7 +19,10 @@ class MapActivityD: AppCompatActivity() {
     private var butka: Button? = null
     private var ArrayZero= arrayOf("Д-001","Д-002","Д-003","Д-004","Д-005","Д-006","Д-007","Д-008","Д-010","Д-011","Д-012","Д-013","Д-013/1","Д-013/2","Д-013/3","Д-013/4","Д-013/5","Д-013/6","Д-013/7","Д-014","Д-015","Д-016","Д-017","Д-017/1","Д-017/2","Д-018","Д-019","Д-020","Д-020/1")
     private var ArrayFirts= arrayOf("Д-101","Д-102","Д-103","Д-103/1","Д-104","Д-105","Д-106","Д-107","Д-108","Д-109","Д-110","Д-111","Д-112","Д-113","Д-113/1","Д-113/2","Д-117","Д-118","Д-119","Д-119/1","Д-125")
-    private var ArraySecond= arrayOf("Д-201","Д-202","Д-203","Д-204","Д-204/1","Д-204/2","Д-205","Д-206","Д-207","Д-208","Д-208/1","Д-209","Д-210","Д-211","Д-211/1","Д-212","Д-212/1","Д-212/2","Д-212/3","Д-213","Д-214","Д-214/2","Д-215","Д-216","Д-216/1","Д-218","Д-219","Д-220","Д-221","Д-222","Д-223","Д-224","Д-225","Д-226","Д-226/1","Д-226/2")
+    private var ArraySecond= arrayOf("Д-201","Д-202","Д-203","Д-204","Д-204/1","Д-204/2","Д-205","Д-206","Д-207","Д-208","Д-208/1","Д-209","Д-210","Д-211","Д-211/1","Д-211/2","Д-212","Д-212/1","Д-212/3","Д-213","Д-214","Д-214/2","Д-215","Д-216","Д-216/1","Д-218","Д-219","Д-220","Д-221","Д-222","Д-223","Д-224","Д-225","Д-226","Д-226/1","Д-226/2")
+    private var ArrayThird= arrayOf("Д-301","Д-302","Д-302/1","Д-303","Д-303/1","Д-304","Д-305","Д-306","Д-307","Д-307/1","Д-308","Д-309","Д-310","Д-311","Д-312","Д-313","Д-314","Д-315","Д-316","Д-317","Д-318","Д-318/1","Д-318/3")
+    private var ArrayFourth= arrayOf("Д-401","Д-402","Д-403","Д-404","Д-405","Д-406","Д-407","Д-408","Д-409","Д-410","Д-411","Д-412","Д-413","Д-413/2","Д-414","Д-415","Д-415/1","Д-415/2","Д-416","Д-417","Д-418","Д-419","Д-419/1")
+    private var ArrayFifth= arrayOf("Д-501","Д-502","Д-503","Д-504","Д-505","Д-506","Д-507","Д-508","Д-509","Д-509/1","Д-510","Д-511","Д-512","Д-513","Д-514","Д-515","Д-516","Д-516/A","Д-517","Д-517/1","Д-518","Д-519","Д-520","Д-521","Д-522","Д-523","Д-524","Д-524/1","Д-525")
     private var baton: Button?=null
     private var zerobut: Button?=null
     private var l=0
@@ -90,6 +93,27 @@ class MapActivityD: AppCompatActivity() {
             zerobut=findViewById<Button>(R.id.ButSecond)
             zerobut!!.setEnabled(false)
             lKab=36
+        } else if(view.id==findViewById<Button>(R.id.ButThird).id){
+            l=3
+            findViewById<ImageView>(R.id.D000).setImageResource(R.drawable.thirddkab)
+            enable(view)
+            zerobut=findViewById<Button>(R.id.ButThird)
+            zerobut!!.setEnabled(false)
+            lKab=23
+        } else if(view.id==findViewById<Button>(R.id.ButFourth).id){
+            l=4
+            findViewById<ImageView>(R.id.D000).setImageResource(R.drawable.fourthdkab)
+            enable(view)
+            zerobut=findViewById<Button>(R.id.ButFourth)
+            zerobut!!.setEnabled(false)
+            lKab=23
+        } else if(view.id==findViewById<Button>(R.id.ButFifth).id){
+            l=5
+            findViewById<ImageView>(R.id.D000).setImageResource(R.drawable.fivedkab)
+            enable(view)
+            zerobut=findViewById<Button>(R.id.ButFifth)
+            zerobut!!.setEnabled(false)
+            lKab=29
         }
         flay?.visibility = View.INVISIBLE
         laya?.visibility = View.INVISIBLE
@@ -208,6 +232,75 @@ class MapActivityD: AppCompatActivity() {
                 val resId = resources.getIdentifier((str+(k+1).toString()), "id", packageName)
                 butka = findViewById(resId)
                 butka?.text = ArraySecond[k].toString()
+                k+=1
+            }
+        }
+        if(l==3){
+            var k=1
+            var str="D_"
+            while(k<=lKab){
+                val resId = resources.getIdentifier((str+k.toString()), "id", packageName)
+                butka = findViewById(resId)
+                butka?.visibility=View.VISIBLE
+                k+=1
+            }
+            while(k<=lKabMax){
+                val resId = resources.getIdentifier((str+k.toString()), "id", packageName)
+                butka = findViewById(resId)
+                butka?.visibility=View.INVISIBLE
+                k+=1
+            }
+            k=0
+            while (k<lKab){
+                val resId = resources.getIdentifier((str+(k+1).toString()), "id", packageName)
+                butka = findViewById(resId)
+                butka?.text = ArrayThird[k].toString()
+                k+=1
+            }
+        }
+        if(l==4){
+            var k=1
+            var str="D_"
+            while(k<=lKab){
+                val resId = resources.getIdentifier((str+k.toString()), "id", packageName)
+                butka = findViewById(resId)
+                butka?.visibility=View.VISIBLE
+                k+=1
+            }
+            while(k<=lKabMax){
+                val resId = resources.getIdentifier((str+k.toString()), "id", packageName)
+                butka = findViewById(resId)
+                butka?.visibility=View.INVISIBLE
+                k+=1
+            }
+            k=0
+            while (k<lKab){
+                val resId = resources.getIdentifier((str+(k+1).toString()), "id", packageName)
+                butka = findViewById(resId)
+                butka?.text = ArrayFourth[k].toString()
+                k+=1
+            }
+        }
+        if(l==5){
+            var k=1
+            var str="D_"
+            while(k<=lKab){
+                val resId = resources.getIdentifier((str+k.toString()), "id", packageName)
+                butka = findViewById(resId)
+                butka?.visibility=View.VISIBLE
+                k+=1
+            }
+            while(k<=lKabMax){
+                val resId = resources.getIdentifier((str+k.toString()), "id", packageName)
+                butka = findViewById(resId)
+                butka?.visibility=View.INVISIBLE
+                k+=1
+            }
+            k=0
+            while (k<lKab){
+                val resId = resources.getIdentifier((str+(k+1).toString()), "id", packageName)
+                butka = findViewById(resId)
+                butka?.text = ArrayFifth[k].toString()
                 k+=1
             }
         }
